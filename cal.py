368a46a439ce1d843fc918fa988c1e83aff077c4
YihRae <yihrae@cumt.edu.cn>
2022-10-27 20:07:08 +0800

    增加了time_graph统计拥堵起点终点功能

--- cal.py
+++ cal.py
@@ -6,10 +6,11 @@ def dict_to_csv(dtc, idx1, idx2, durl):
     dtc_k = dtc.keys()
     dtc_v = dtc.values()
     dtc_t = {idx1: dtc_k, idx2: dtc_v}
-    pd.DataFrame(dtc_t).to_csv(durl, encoding='utf-8')
+    pd.DataFrame(dtc_t).sort_values(by=[idx1], ascending=False)\
+        .reset_index(drop=True).to_csv(durl, encoding='utf-8')
 
 
-file_name = './data/dataset/GM_speed/result/result_0617.csv'
+file_name = './data/result_speed/result_11.csv'
 a = []
 b = []
 df = pd.read_csv(file_name, index_col=0)
@@ -22,4 +23,4 @@ for i in range(k):
             a[i][x] = 1
         else:
             a[i][x] += 1
-    dict_to_csv(a[i], df.columns[i], 'N', df.columns[i] + '17.csv')
+    dict_to_csv(a[i], df.columns[i], 'N', df.columns[i] + '411.csv')
--- time_graph.py
+++ time_graph.py
@@ -2,10 +2,13 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import xlwt
 
 
 def divide_graph(n_data, h_time, t_time):
     slice_class = n_data['TIMESLICE'].unique()
+    h_time = min(h_time, len(slice_class) - 1)
+    t_time = min(t_time, len(slice_class) - 1)
     divide_net = [n_data[n_data['TIMESLICE'].isin([c])].reset_index(drop=True)
                   for c in slice_class]
     return slice_class[h_time], slice_class[t_time], divide_net[h_time: t_time + 1]
@@ -13,8 +16,8 @@ def divide_graph(n_data, h_time, t_time):
 
 def build(v_size, e_data, e_id, n_data, h_time, t_time):
     edge_ret = []
-    time_len = t_time - h_time + 1
     t_min, t_max, divide_net = divide_graph(n_data, h_time, t_time)
+    time_len = t_max - t_min + 1
 
     def cal_idx(idx, timeslice, vsize):
         return idx + vsize * timeslice
@@ -23,7 +26,7 @@ def build(v_size, e_data, e_id, n_data, h_time, t_time):
         edge_ret[u].append((v, w))
 
     dict_edge = {}
-    edge_ret = [[] for i in range(v_size * time_len + 5)]
+    edge_ret = [[] for _ in range(v_size * time_len + 5)]
     for i in range(e_id.shape[0]):
         dict_edge[e_id.loc[i, 'ID']] = e_id.loc[i, 'INDEX']
     for i in range(1, time_len):
@@ -37,7 +40,7 @@ def build(v_size, e_data, e_id, n_data, h_time, t_time):
             cur_idx = int(dict_edge.get(cur_id, '0'))
             if cur_idx == 0:
                 continue
-            if e_data.loc[cur_idx, 'JAM'] > net_part.loc[i, 'SPEED']:
+            if e_data.loc[cur_idx, 'JAM'] - net_part.loc[i, 'SPEED'] > 0:
                 cur_fr = e_data.loc[cur_idx, 'FR']
                 cur_to = e_data.loc[cur_idx, 'TO']
                 add(cal_idx(cur_fr, t_slice, v_size),
@@ -45,7 +48,7 @@ def build(v_size, e_data, e_id, n_data, h_time, t_time):
     return t_min, t_max, time_len, edge_ret
 
 
-def cal_res(t_len, init_vsize, edge, file_n, file_save):
+def cal_res(t_len, init_vsize, edge, file_n, file_save, v_c_cnt):
     v_size = init_vsize * t_len
     vis = [False for _ in range(v_size)]
     ret = []
@@ -53,7 +56,6 @@ def cal_res(t_len, init_vsize, edge, file_n, file_save):
     c_st = []
     c_ed = []
     t_data = []
-    tmp = {}
     for ver in range(v_size):
         if vis[ver]:
             continue
@@ -65,7 +67,6 @@ def cal_res(t_len, init_vsize, edge, file_n, file_save):
         ed_tmp = ''
         while len(st) > 0:
             cur = st[-1]
-            tmp[cur % t_len] = 1
             st.pop()
             flag = False
             for e in edge[cur]:
@@ -85,30 +86,71 @@ def cal_res(t_len, init_vsize, edge, file_n, file_save):
                     vis[lst] = True
                     st.append(lst)
         if res > 0:
+            cur_id = ver % t_len
+            v_c_cnt[cur_id].append(res)
             c_ed.append(ed_tmp)
             t_data.append(cur_tmax - cur_tmin + 1)
-            c_st.append(ver % t_len)
+            c_st.append(cur_id)
             ret.append(res)
-    print(len(tmp))
     df = pd.DataFrame({'F': ret, 'T': t_data, 'S': c_st, 'E': c_ed})
     df.to_csv(file_save + '/result_' + file_n + '.csv')
     sns.distplot(df['F'], hist=True, kde=True, bins=500)
     plt.savefig(file_save + "/frequency_" + file_n + '.jpg')
-    plt.show()
+    # plt.show()
 
 
 if __name__ == '__main__':
-    f_name = './data/dataset/GM_speed/GM_road_speed_data' + '_' + '0617' + '.csv'
-    head_time = 0
-    tail_time = 285
-    # 0615时间片不全 0620数据极少 0619和0620为周末其余为工作日
-    net_data = pd.read_csv(f_name, usecols=['SPEED', 'DATE',
-                                            'TIMESLICE', 'ID'])
+    file_head = './data/dataset/speed/road_speed_data_4-'
+    file_list = []
+    for i in range(6, 20):
+        if i < 10:
+            file_list.append(file_head + '0' + str(i) + '.csv')
+        else:
+            file_list.append(file_head + str(i) + '.csv')
     # 读取路网
     vertex_data = pd.read_csv('data/graph/vertex_data.csv', index_col=0)
     edge_data = pd.read_csv('data/graph/edge_data.csv', index_col=0)
     edge_id = pd.read_csv('data/graph/edge_id.csv', index_col=0)
     vertex_size = vertex_data.shape[0]
-    time_min, time_max, slice_len, edge_set = build(vertex_size, edge_data, edge_id,
-                                                    net_data, head_time, tail_time)
-    cal_res(tail_time - head_time + 1, vertex_size, edge_set, '0617', './data/result_GM')
+    head_time = 0
+    tail_time = 287
+    # 0615时间片不全 0620数据极少 0619和0620为周末其余为工作日
+    net_data = [pd.read_csv(f_name, usecols=['SPEED', 'DATE',
+                                             'TIMESLICE', 'ID']) for f_name in file_list]
+    cnt = 5
+    vertex_cluster = [[] for _ in range(vertex_size + 5)]
+    for net in net_data:
+        time_min, time_max, slice_len, edge_set = build(vertex_size, edge_data, edge_id,
+                                                        net, head_time, tail_time)
+        cnt += 1
+        cal_res(time_max - time_min + 1, vertex_size, edge_set, str(cnt),
+                './data/result_speed', vertex_cluster)
+    xl =[xlwt.Workbook() for _ in range((vertex_size + 19) // 20 + 2)]
+    xl_sh = xl[0].add_sheet('result')
+    cnt = 0
+    xlid = 0
+    for i in range(vertex_size):
+        num = sum(vertex_cluster[i])
+        if num == 0:
+            continue
+        xl_sh.write(0, cnt * 2, i)
+        xl_sh.write(1, cnt * 2, num)
+        d_tmp = {}
+        for j in range(len(vertex_cluster[i])):
+            if d_tmp.get(vertex_cluster[i][j], 0) == 0:
+                d_tmp[vertex_cluster[i][j]] = 1
+            else:
+                d_tmp[vertex_cluster[i][j]] += 1
+        num = 0
+        for d_key in d_tmp:
+            xl_sh.write(num + 2, cnt * 2, d_key)
+            xl_sh.write(num + 2, cnt * 2 + 1, d_tmp[d_key])
+            num += 1
+        cnt += 1
+        if cnt == 20:
+            cnt = 0
+            xl[xlid].save('./data/result_speed/cluster' + str(xlid) + '.xls')
+            xlid += 1
+            xl_sh = xl[xlid].add_sheet('result')
+    if cnt > 0:
+        xl[xlid].save('./data/result_speed/cluster' + str(xlid) + '.xls')
